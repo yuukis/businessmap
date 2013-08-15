5e43b99a700ddf891d74ed6eef8be449708a9f12
Yuuki SHIMIZU <yuuki@maxio.jp>
2013-08-15 22:07:38 +0900

    Fragment の処理を Activity に移行

--- src/com/github/yuukis/businessmap/ContactsListFragment.java
+++ src/com/github/yuukis/businessmap/ContactsListFragment.java
@@ -1,20 +1,9 @@
 package com.github.yuukis.businessmap;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 import android.app.ListFragment;
-import android.content.Context;
-import android.database.Cursor;
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
-import android.os.Handler;
-import android.provider.ContactsContract.CommonDataKinds.GroupMembership;
-import android.provider.ContactsContract.CommonDataKinds.StructuredPostal;
-import android.provider.ContactsContract.Data;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -22,108 +11,23 @@ import android.widget.TextView;
 
 public class ContactsListFragment extends ListFragment {
 
-	private List<ContactsItem> mContactsList;
 	private ContactsAdapter mContactsAdapter;
-	private Handler mHandler = new Handler();
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 
-		mContactsList = new ArrayList<ContactsItem>();
 		mContactsAdapter = new ContactsAdapter();
 		setListAdapter(mContactsAdapter);
 	}
 
-	public void loadContactsByGroupId(long gid) {
-
-		Context context = getActivity();
-
-		Cursor groupCursor = context.getContentResolver().query(
-				Data.CONTENT_URI,
-				new String[]{
-						GroupMembership.RAW_CONTACT_ID,
-						GroupMembership.DISPLAY_NAME },
-				Data.MIMETYPE + "=? AND " +
-						GroupMembership.GROUP_ROW_ID + "=?",
-				new String[] {
-						GroupMembership.CONTENT_ITEM_TYPE,
-						String.valueOf(gid)},
-				Data.RAW_CONTACT_ID);
-
-		Cursor postalCursor = context.getContentResolver().query(
-				StructuredPostal.CONTENT_URI,
-				new String[] {
-						StructuredPostal.RAW_CONTACT_ID,
-						StructuredPostal.FORMATTED_ADDRESS },
-				null,
-				null,
-				StructuredPostal.RAW_CONTACT_ID);
-
-		CursorJoinerWithIntKey joiner = new CursorJoinerWithIntKey(
-				groupCursor, new String[] { Data.RAW_CONTACT_ID },
-				postalCursor, new String[] { Data.RAW_CONTACT_ID });
-
-		mContactsList.clear();
-
-		for (CursorJoinerWithIntKey.Result result : joiner) {
-			String name, address;
-
-			switch (result) {
-			case LEFT:
-				name = groupCursor.getString(1);
-				address = null;
-				break;
-
-			case BOTH:
-				name = groupCursor.getString(1);
-				address = postalCursor.getString(1);
-				break;
-
-			default:
-				continue;
-			}
-
-			mContactsList.add(new ContactsItem(name, address));
-		}
-
-		groupCursor.close();
-		postalCursor.close();
-
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				findLatLng();
-				mHandler.post(new Runnable() {
-					@Override
-					public void run() {
-						mContactsAdapter.notifyDataSetChanged();
-					}
-				});
-			}
-		}).start();
+	public void notifyDataSetChanged() {
+		mContactsAdapter.notifyDataSetChanged();
 	}
 
-	private void findLatLng() {
-		int listSize = mContactsList.size();
-		for (int i = 0; i < listSize; i++) {
-			ContactsItem contact = mContactsList.get(i);
-			String address = contact.getAddress();
-			if (address == null) {
-				continue;
-			}
-			try {
-				List<Address> list = new Geocoder(getActivity(),
-						Locale.getDefault()).getFromLocationName(address, 1);
-				if (list.size() > 0) {
-					Address addr = list.get(0);
-					contact.setLat(addr.getLatitude());
-					contact.setLng(addr.getLongitude());
-					mContactsList.set(i, contact);
-				}
-			} catch (IOException e) {
-			}
-		}
+	private List<ContactsItem> getContactsList() {
+		MainActivity activity = (MainActivity) getActivity();
+		return activity.getContactsList();
 	}
 
 	private static class ViewHolder {
@@ -133,14 +37,16 @@ public class ContactsListFragment extends ListFragment {
 
 	private class ContactsAdapter extends BaseAdapter {
 
+
+
 		@Override
 		public int getCount() {
-			return mContactsList.size();
+			return getContactsList().size();
 		}
 
 		@Override
 		public Object getItem(int position) {
-			return mContactsList.get(position);
+			return getContactsList().get(position);
 		}
 
 		@Override
--- src/com/github/yuukis/businessmap/MainActivity.java
+++ src/com/github/yuukis/businessmap/MainActivity.java
@@ -1,12 +1,21 @@
 package com.github.yuukis.businessmap;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.ContactsContract.Data;
 import android.provider.ContactsContract.Groups;
+import android.provider.ContactsContract.CommonDataKinds.GroupMembership;
+import android.provider.ContactsContract.CommonDataKinds.StructuredPostal;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.database.Cursor;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
@@ -14,12 +23,19 @@ import android.widget.ArrayAdapter;
 public class MainActivity extends Activity implements
 		ActionBar.OnNavigationListener {
 
-	List<ContactsGroup> mGroupList;
+	private List<ContactsGroup> mGroupList;
+	private List<ContactsItem> mContactsList;
+	private ContactsListFragment mListFragment;
+	private Handler mHandler = new Handler();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		FragmentManager fm = getFragmentManager();
+		mListFragment = (ContactsListFragment) fm
+				.findFragmentById(R.id.contacts_list);
 
 		Cursor groupCursor = getContentResolver().query(
 				Groups.CONTENT_URI,
@@ -32,6 +48,7 @@ public class MainActivity extends Activity implements
 				null);
 
 		mGroupList = new ArrayList<ContactsGroup>();
+		mContactsList = new ArrayList<ContactsItem>();
 
 		while (groupCursor.moveToNext()) {
 			long _id = groupCursor.getLong(0);
@@ -62,9 +79,100 @@ public class MainActivity extends Activity implements
 			return false;
 		}
 		ContactsGroup group = mGroupList.get(itemPosition);
-		((ContactsListFragment) getFragmentManager().findFragmentById(
-				R.id.contacts_list)).loadContactsByGroupId(group.getId());
+		loadContactsByGroupId(group.getId());
 		return true;
+	}
+
+	public void loadContactsByGroupId(long gid) {
+		Cursor groupCursor = getContentResolver().query(
+				Data.CONTENT_URI,
+				new String[]{
+						GroupMembership.RAW_CONTACT_ID,
+						GroupMembership.DISPLAY_NAME },
+				Data.MIMETYPE + "=? AND " +
+						GroupMembership.GROUP_ROW_ID + "=?",
+				new String[] {
+						GroupMembership.CONTENT_ITEM_TYPE,
+						String.valueOf(gid)},
+				Data.RAW_CONTACT_ID);
+
+		Cursor postalCursor = getContentResolver().query(
+				StructuredPostal.CONTENT_URI,
+				new String[] {
+						StructuredPostal.RAW_CONTACT_ID,
+						StructuredPostal.FORMATTED_ADDRESS },
+				null,
+				null,
+				StructuredPostal.RAW_CONTACT_ID);
+
+		CursorJoinerWithIntKey joiner = new CursorJoinerWithIntKey(
+				groupCursor, new String[] { Data.RAW_CONTACT_ID },
+				postalCursor, new String[] { Data.RAW_CONTACT_ID });
+
+		mContactsList.clear();
+
+		for (CursorJoinerWithIntKey.Result result : joiner) {
+			String name, address;
+
+			switch (result) {
+			case LEFT:
+				name = groupCursor.getString(1);
+				address = null;
+				break;
+
+			case BOTH:
+				name = groupCursor.getString(1);
+				address = postalCursor.getString(1);
+				break;
+
+			default:
+				continue;
+			}
+
+			mContactsList.add(new ContactsItem(name, address));
+		}
+
+		groupCursor.close();
+		postalCursor.close();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				findLatLng();
+				mHandler.post(new Runnable() {
+					@Override
+					public void run() {
+						mListFragment.notifyDataSetChanged();
+					}
+				});
+			}
+		}).start();
+	}
+
+	public List<ContactsItem> getContactsList() {
+		return mContactsList;
+	}
+
+	private void findLatLng() {
+		int listSize = mContactsList.size();
+		for (int i = 0; i < listSize; i++) {
+			ContactsItem contact = mContactsList.get(i);
+			String address = contact.getAddress();
+			if (address == null) {
+				continue;
+			}
+			try {
+				List<Address> list = new Geocoder(this,
+						Locale.getDefault()).getFromLocationName(address, 1);
+				if (list.size() > 0) {
+					Address addr = list.get(0);
+					contact.setLat(addr.getLatitude());
+					contact.setLng(addr.getLongitude());
+					mContactsList.set(i, contact);
+				}
+			} catch (IOException e) {
+			}
+		}
 	}
 
 }
