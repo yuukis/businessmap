a5e8edd0195837e054b3e2662ebc74787b86b45b
Yuuki Shimizu <yuuki@maxio.jp>
2014-04-15 14:29:04 +0900

    [171] fix #24

--- src/com/github/yuukis/businessmap/app/ContactsTaskFragment.java
+++ src/com/github/yuukis/businessmap/app/ContactsTaskFragment.java
@@ -149,11 +149,16 @@ public class ContactsTaskFragment extends Fragment {
 		args.putString(ProgressDialogFragment.MESSAGE, message);
 		args.putBoolean(ProgressDialogFragment.CANCELABLE, true);
 		args.putInt(ProgressDialogFragment.MAX, max);
-		DialogFragment dialog = ProgressDialogFragment.newInstance();
+		final DialogFragment dialog = ProgressDialogFragment.newInstance();
 		dialog.setArguments(args);
 		if (getActivity() != null) {
-			dialog.show(getActivity().getFragmentManager(),
-					ProgressDialogFragment.TAG);
+			new Handler(Looper.getMainLooper()).post(new Runnable() {
+				@Override
+				public void run() {
+					dialog.show(getActivity().getFragmentManager(),
+							ProgressDialogFragment.TAG);
+				}
+			});
 		}
 	}
 
@@ -185,10 +190,10 @@ public class ContactsTaskFragment extends Fragment {
 				@Override
 				public void run() {
 					new AlertDialog.Builder(getActivity())
-					.setTitle(title)
-					.setMessage(message)
-					.setPositiveButton(android.R.string.ok, null)
-					.show();
+							.setTitle(title)
+							.setMessage(message)
+							.setPositiveButton(android.R.string.ok, null)
+							.show();
 				}
 			});
 		}
